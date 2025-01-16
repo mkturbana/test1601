987219fc9f00b5e6c87d5fa3caa4d77d23ba41c6
mkturbana <mkturbana@gmail.com>
2025-01-16 17:31:14 -0300

    Delete hello.js

--- hello.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === "GET") {
-    res.status(200).json({ message: "API funcionando!" });
-  } else {
-    res.status(405).json({ error: "Método não permitido" });
-  }
-}

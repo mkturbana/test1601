cb11130b309fc8202ad91db3256ef89b008479cc
mkturbana <mkturbana@gmail.com>
2025-01-17 11:15:49 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Bem-vindo ao meu projeto!</h1>
+      <p>Use os endpoints em <code>/api</code> para acessar a API.</p>
+    </div>
+  );
+}
